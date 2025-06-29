3bbd00d55e044028f00e96d495cb7a8139703499
VatsalM78 <157975822+VatsalVashishthaGithub@users.noreply.github.com>
2025-06-29 18:08:37 +0530

    Create (L.C.-1207) Unique Number of Occurrences.cpp

---
+++ (L.C.-1207) Unique Number of Occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int> mpp;
+        for(auto x : arr){
+            mpp[x]++;
+        }
+        vector<int> vec;
+        for(auto i : mpp){
+            vec.push_back(i.second);
+        }
+        sort(vec.begin(),vec.end());
+        for(int i=1; i<vec.size(); i++){
+            if(vec[i] == vec[i-1]) return false;
+        }
+        return true;
+    }
+};
